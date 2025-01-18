b2b2b86ecb2f80649bfc1f7dcd3bdf7509113408
sambitsekharparida <111132029+sambitsekharparida@users.noreply.github.com>
2025-01-18 21:11:53 +0000

    variables

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  // alert works with browser not with nodeJS
+
+let name = "Sambit" // string => "" / ''
+let age = 30 //number (2 to the power 53)
+let isLoggedIn = true // boolean => true/false
+let secondName = null // null is a standlone value 
+let contactNumber; // undefined => value is not defined
+
+// null and undefined is different from each other
+
+// symbol => to represent unique 
+// object
+
+console.log(typeof "Sambit") // string
+console.log(typeof age) // number
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
